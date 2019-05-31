5f6b6348174aad247497d050521d96562438b84a
williammango <williammango2015@gmail.com>
2019-05-31 12:42:00 +0300

    create Quotes form

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
-}
+  constructor(
+    public quote: string,
+    public author: string,
+    public quoter: string, ) { }
+}//create constructor function and define properties for each instance ofthe class
