e9d4de16b4904d41089e299071905c068a86581a
williammango <williammango2015@gmail.com>
2019-05-31 14:31:35 +0300

    replace app html content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuotesformComponent } from './quotesform/quotesform.component';
@@ -14,7 +15,9 @@ import { QuotesDetailsComponent } from './quotes-details/quotes-details.componen
     QuotesDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
