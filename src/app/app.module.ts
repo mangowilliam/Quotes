4c37f35b274061083dec0abaaa265d91a9f9d775
williammango <williammango2015@gmail.com>
2019-05-31 13:58:34 +0300

    greate quotes-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuotesformComponent } from './quotesform/quotesform.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesformComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule
