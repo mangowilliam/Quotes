b4c7744fd162f1ea80a7ee68a8090925ed457a61
williammango <williammango2015@gmail.com>
2019-06-04 09:12:33 +0300

    Auto-generated commit

--- src/app/quotes.ts
+++
@@ -1,7 +0,0 @@
-export class Quotes {
-  public showDescription: boolean;
-  constructor(public id: number, public upvote: number, public downvote: number, public quote: string, public author: string, public quoter: string, public Date: Date) {
-    this.showDescription = false
-  }
-
-}//create constructor function and define properties for each instance ofthe class
